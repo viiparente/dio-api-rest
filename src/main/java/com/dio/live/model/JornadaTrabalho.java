86e7c47b05a62a39ac6079bc5018b7c9f5e15207
Vinicius Parente <vd.parente@hotmail.com>
2021-07-31 12:45:36 -0300

    Changed Using Lombok

---
+++ src/main/java/com/dio/live/model/JornadaTrabalho.java
@@ -0,0 +1,15 @@
+package com.dio.live.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+//@Data
+public class JornadaTrabalho {
+    private long id;
+    private String descricao;
+}
